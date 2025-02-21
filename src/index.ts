307a560533e0b6a0fc029c06d9cf351fad19eb21
stonepig <1048218204@qq.com>
2025-02-21 14:26:51 +0800

    利用 XMLHttpRequest 发送请求

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-function axios(config) {}
+import { AxiosRequestConfig } from './types'
+import xhr from './xhr'
+
+function axios(config: AxiosRequestConfig): void {
+  xhr(config)
+}
 
 export default axios
