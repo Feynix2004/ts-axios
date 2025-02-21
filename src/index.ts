4283b6d1929c56548104f25c3defc5867524666e
stonepig <1048218204@qq.com>
2025-02-21 14:47:47 +0800

    处理请求body数据

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { transformRequest } from './helpers/data'
 import { bulidURL } from './helpers/url'
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
@@ -9,6 +10,7 @@ function axios(config: AxiosRequestConfig): void {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  config.data = transformRequestData(config)
 }
 
 function transformUrl(config: AxiosRequestConfig): string {
@@ -16,3 +18,7 @@ function transformUrl(config: AxiosRequestConfig): string {
   return bulidURL(url, params)
 }
 export default axios
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
